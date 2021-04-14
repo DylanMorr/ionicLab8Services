b287c70a023fa62677d320665cd853c5a3475261
Dylan Morrow <G00365708@gmit.ie>
2021-04-14 15:15:22 +0100

    Lab Complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MovieService} from '../Services/movie.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  movies: any = [];
+  constructor(private movieService:MovieService) {}
 
-  constructor() {}
+  ngOnInit(){
+    // .subscribe makes it async so the pages doesnt freeze while looking for data
+    this.movieService.GetMovieData().subscribe(
+      (data)=>{
+        this.movies = data.Search;
+        // can inspect page to see api data with console.log
+        console.log(this.movies);
+      }
+    );
+  }
 
 }
